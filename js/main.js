001020323d41408c25e24e0bfed5303a45e0fef3
HelenZhilinskaya <helen.zhilinskaya@gmail.com>
2017-01-18 18:14:17 +0300

    Delete main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-
-//.parallax(xPosition, speedFactor, outerHeight) options:
-//xPosition - Horizontal position of the element
-//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
-$('#intro').parallax("50%", 0.1);
-$('#second').parallax("50%", 0.1);
-$('#third').parallax("50%", 0.3);
-$('#footer').parallax("50%", 0.3);
-
